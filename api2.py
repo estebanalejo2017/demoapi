0906d13d1394cd40ad80ec0bcf46a9b6880fabcb
Esteban <estebanalejo2017@hotmail.com>
2023-06-16 01:44:51 -0500

    version completa de apis, con yahoo y wallapop, mostrando como json todo y con estados html

--- api2.py
+++ api2.py
@@ -1,7 +1,8 @@
 #creacion de api con Flask importando query
-from flask import Flask,request
+from flask import Flask,request,Response
 from query import get_company_information
 from query_wallapop import get_wallapop
+import json
 
 
 app = Flask(__name__)
@@ -11,15 +12,17 @@ def hello_world():
 
 @app.route("/api/yahoo")
 def get_company():
-    symbol = "AAPL"
-    return get_company_information(symbol)
+    symbol = request.args.get('symbol')
+    if symbol is None:
+        return Response(json.dumps({"error": "missing 'symbol' query parameter"}),status=400,mimetype='application/json')
+    return Response(json.dumps(get_company_information(symbol)),status=200,mimetype='application/json')
 
 @app.route("/api/wallapop")
 def get_product_and_price_average():
     query = request.args.get('search_text')
     if query is None:
-        return "empty query"
-    return get_wallapop(query)
+        return Response(json.dumps({"error": "missing 'search_text' query parameter"}),status=400,mimetype='application/json')
+    return Response(json.dumps(get_wallapop(query)),status=400,mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
